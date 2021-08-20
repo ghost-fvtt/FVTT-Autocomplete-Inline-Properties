628e8c1fa726c695496c18f5eb5c3cda4264894c
Johannes Loher <johannes.loher@fg4f.de>
2021-08-20 23:36:18 +0200

    ci: automatically update module.json

---
+++ .versionrc.js
@@ -0,0 +1,18 @@
+const manifest = {
+    filename: "module.json",
+    updater: require("./manifest-version-updater"),
+};
+
+const package = {
+    filename: "package.json",
+    type: "json",
+};
+
+const packageLock = {
+    filename: "package-lock.json",
+    type: "json",
+};
+
+module.exports = {
+    bumpFiles: [package, packageLock, manifest],
+};
