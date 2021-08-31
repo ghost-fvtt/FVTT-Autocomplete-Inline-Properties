9e6043a2d5c632cc482a196ffa1ccaba2fb0cb1b
Johannes Loher <johannes.loher@fg4f.de>
2021-08-31 23:45:24 +0200

    refactor: add proper logging functionality

--- modules/const.mjs
+++ modules/const.mjs
@@ -1,1 +1,2 @@
 export const MODULE_NAME = "autocomplete-inline-properties";
+export const MODULE_ABBREVIATION = "AIP";
---
+++ modules/logger.mjs
@@ -0,0 +1,36 @@
+import { MODULE_ABBREVIATION } from "./const.mjs";
+
+const loggingContext = MODULE_ABBREVIATION;
+const loggingSeparator = "|";
+
+/**
+ * Gets a logging function for the requested log level.
+ * @typedef {'debug' | 'info' | 'warn' | 'error'} LogLevel
+ * @typedef {(...args: unknown[]) => void} LoggingFunction
+ * @param {LogLevel} [type = 'info'] - The log level of the requested logger
+ * @returns {LoggingFunction}
+ */
+function getLoggingFunction(type = "info") {
+    const log = console[type];
+    if (type === "debug") {
+        return (...data) => {
+            if (CONFIG.debug.aip) {
+                log(loggingContext, loggingSeparator, ...data);
+            }
+        };
+    }
+    return (...data) => log(loggingContext, loggingSeparator, ...data);
+}
+
+/**
+ * A singleton logger object.
+ */
+const logger = Object.freeze({
+    debug: getLoggingFunction("debug"),
+    info: getLoggingFunction("info"),
+    warn: getLoggingFunction("warn"),
+    error: getLoggingFunction("error"),
+    getLoggingFunction,
+});
+
+export default logger;
